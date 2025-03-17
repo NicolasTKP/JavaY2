892c25e56a1efa269af3358863ce342cac2a8b6f
Nicolas <tankerping@gmail.com>
2025-03-17 11:27:42 +0800

    Added Login.java

--- JavaY2.java
+++ JavaY2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.javay2;
+package com.mycompany.JavaY2;
 
 /**
  *
---
+++ Login.java
@@ -0,0 +1,4 @@
+package com.mycompany.JavaY2;
+
+public class Login {
+}
