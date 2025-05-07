cf9bfa7286b6d22de6c51b83c2fa41ef7c08471e
Kelvin <vuihung6898@gmail.com>
2025-05-08 02:28:08 +0800

    Add item mainpage Supplier mainpage and supplier class

--- Class/DataMapping.java
+++ Class/DataMapping.java
@@ -33,5 +33,26 @@ public class DataMapping {
             System.out.println("Error loading file: " + e.getMessage());
         }
         return mapping;
+    }
+
+    public Map<String, String> NameIdMapping(String file_path) {
+        Map<String, String> mapping = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file_path))) {
+            String line;
+            br.readLine(); // Skip header
+
+            while ((line = br.readLine()) != null) {
+                String[] columns = line.split("\\|");
+                if (columns.length >= 2) {
+                    String id = columns[0];
+                    String name = columns[1];
+                    mapping.put(name,id);
+                }
+            }
+        } catch(IOException e) {
+            System.out.println("Error loading file: " + e.getMessage());
+        }
+        return mapping;
     }    
+    
 }
--- Object/Supplier.java
+++ Object/Supplier.java
@@ -1,5 +1,6 @@
 package com.mycompany.JavaY2.Object;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -34,5 +35,124 @@ public class Supplier {
                 this.payment_term.toLowerCase()
         ));
         return valuesToCheck.contains(keyword);
+    }
+    
+    public static String getNextSupplierID() {
+        String last_supplier_id = null;
+        String supplier_file_path = "src\\main\\java\\com\\mycompany\\JavaY2\\TextFile\\suppliers";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(supplier_file_path))) {
+            br.readLine();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                last_supplier_id = line.split("\\|")[0]; // Get first column (item_id)
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading supplier text file");
+            return null;
+        }
+
+        // If no lastItemId found, return the first ID as "I001"
+        if (last_supplier_id == null) {
+            return "SU001";
+        }
+
+        // Extract numeric part and increment
+        if (last_supplier_id.startsWith("SU")) {
+            try {
+                int lastNumber = Integer.parseInt(last_supplier_id.substring(2)); // Extract numeric part
+                int newNumber = lastNumber + 1; // Increment
+                return String.format("SU%03d", newNumber); // Format as I002, I003, etc.
+            } catch (NumberFormatException e) {
+                System.out.println("Error in assigning Supplier ID to supplier.");
+            }
+        }
+        return null;
+    }
+ 
+    public String setSupplierID(){
+        this.supplier_id = getNextSupplierID();
+        return supplier_id;
+    }    
+    
+    public String getSupplierID(){
+        return supplier_id;
+    }
+    
+    public void setSupplierName(String supplier_name){
+        this.supplier_name = supplier_name;
+    }
+    
+    public String getSupplierName(){
+        return supplier_name;
+    }
+    
+    public void setAddress(String address){
+        this.address = address;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public void setContactNumber(String contact){
+        this.contact = contact;
+    }
+    
+    public String getContactNumber(){
+        return contact;
+    }
+    
+    public void setSupplyItem(String item_id){
+        this.supply_items = item_id;
+    }
+    
+    public String getSupplyItem(){
+        return supply_items;
+    }
+    
+    public void setPaymentTerm(String payment_term){
+        this.payment_term = payment_term;
+    }
+    
+    public String getPaymentTerm(){
+        return payment_term;
+    }
+    
+    public static boolean checkIsNewItem(String item_id){
+        String item_file_path = "src\\main\\java\\com\\mycompany\\JavaY2\\TextFile\\items";
+        try (BufferedReader br = new BufferedReader(new FileReader(item_file_path))) {
+            br.readLine();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] columns = line.split("\\|"); // Get first column (item_id)
+                if(columns.length >=1 && item_id.equalsIgnoreCase(columns[1])){
+                    return false;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading item text file");
+        }
+        return true;                
+    }
+    
+    public static boolean checkIsDuplicateItem(String supplier_id, String item_id){
+       String item_file_path = "src\\main\\java\\com\\mycompany\\JavaY2\\TextFile\\items";
+        try (BufferedReader br = new BufferedReader(new FileReader(item_file_path))) {
+            br.readLine();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] columns = line.split("\\|"); // Get first column (item_id)
+                if(columns.length >=1 && supplier_id.equalsIgnoreCase(columns[6]) && item_id.equalsIgnoreCase(columns[0])){
+                    return false;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading item text file");
+        }
+        return true;        
     }
 }
