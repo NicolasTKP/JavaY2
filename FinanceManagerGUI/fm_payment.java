6694af27a805ac27b9d75262de600f596b876cec
bryan0103 <ooiyiyang123@gmail.com>
2025-06-02 18:27:50 +0800

    push to yy branch

--- FinanceManagerGUI/fm_payment.java
+++ FinanceManagerGUI/fm_payment.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.JavaY2.FinanceManagerGUI;
 
+import com.mycompany.JavaY2.AdminGUI.admin_profile;
 import com.mycompany.JavaY2.Class.Edit;
 import com.mycompany.JavaY2.Object.SessionManager;
 import java.io.BufferedReader;
@@ -321,7 +322,7 @@ public class fm_payment extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "Wrong password, you're not allowed to access the Profile page", "Warning", JOptionPane.WARNING_MESSAGE);
             return;
         }
-        new fm_profile().setVisible(true);
+        new admin_profile().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_btnProfileActionPerformed
 
--- FinanceManagerGUI/fm_purchase_requisitions.java
+++ FinanceManagerGUI/fm_purchase_requisitions.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.JavaY2.FinanceManagerGUI;
 
+import com.mycompany.JavaY2.AdminGUI.admin_profile;
 import com.mycompany.JavaY2.Object.SessionManager;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -217,7 +218,7 @@ public class fm_purchase_requisitions extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "Wrong password, you're not allowed to access the Profile page", "Warning", JOptionPane.WARNING_MESSAGE);
             return;
         }
-        new fm_profile().setVisible(true);
+        new admin_profile().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_btnProfileActionPerformed
 
